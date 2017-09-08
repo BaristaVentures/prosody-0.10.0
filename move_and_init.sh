4ba3af3e258bbad754e03b502d5d2ff4974bfe8c
edwaraco <edward.alejandro.cortes@gmail.com>
2017-09-08 08:52:29 -0500

    Run create_certs.sh only one time to avoid problem when someone tries to run the image.

--- move_and_init.sh
+++ move_and_init.sh
@@ -2,4 +2,11 @@
 cp -a /prosody/etc/* /etc/prosody/
 cp -a /prosody/modules/ /usr/lib/prosody/modules/
 chmod a+x /prosody/scripts/*.sh
-/prosody/scripts/create_cert.sh && /prosody/scripts/create_user.sh && /usr/bin/supervisord
+if [ -e /prosody/scripts/create_cert.txt ]
+then
+    /prosody/scripts/create_user.sh && /usr/bin/supervisord
+else
+    echo "Creating certs..."
+    echo "created!" > /prosody/scripts/create_cert.txt
+    /prosody/scripts/create_cert.sh && /prosody/scripts/create_user.sh && /usr/bin/supervisord
+fi
